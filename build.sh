365db626d2af5e3693f0f6ce5c62b98589e239a2
Santos Gallegos <santos_g@outlook.com>
2016-07-28 23:57:45 -0500

    Update script

--- build.sh
+++ build.sh
@@ -4,7 +4,12 @@ FILES="src/00_portada.md \
     src/00.1_legal.md \
     src/00.2_agredecimientos.md \
     src/01_prologo.md \
-    src/03_ejercicio.md"
+    src/03_ejercicio.md \
+    src/04_ejercicio.md \
+    src/05_ejercicio.md \
+    src/06_ejercicio.md \
+    src/07_ejercicio.md \
+    src/08_ejercicio.md"
 
 OUTPUT="build/edabook.pdf"
 FLAGS="-f markdown_github --toc --chapters -V lang=es"
