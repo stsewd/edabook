f360573f7b1475907b4788b48840d9e64a1a8a16
Santos Gallegos <santos_g@outlook.com>
2016-07-28 19:49:39 -0500

    Compilar usando markdown_github

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 FILES="src/00_portada.md \
     src/00.1_legal.md \
     src/00.2_agredecimientos.md \
-    src/01_prologo.md"
+    src/01_prologo.md \
+    src/03_ejercicio.md"
 
 OUTPUT="build/edabook.pdf"
-FLAGS="--toc --chapters -V lang=es"
+FLAGS="-f markdown_github --toc --chapters -V lang=es"
 
 pandoc -o $OUTPUT $FLAGS $FILES
